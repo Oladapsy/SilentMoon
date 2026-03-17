f3274ef4e9ef1949fe8290cd46a3700dd341ac68
oladapsy <dapoodedeyi@yahoo.com>
2026-03-17 11:10:45 +0100

    font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,50 @@
+import React, { useEffect } from "react";
 import { Stack } from "expo-router";
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
-
 export default function RootLayout() {
-  return <Stack 
-  
-  />;
+  const [fontsLoaded] = useFonts({
+    // Airbnb Cereal
+    "AirbnbCereal-Bold": require("@/assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Bd.otf"),
+    "AirbnbCereal-Light": require("@/assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Lt.otf"),
+    "AirbnbCereal-Book": require("@/assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Bk.otf"),
+    "AirbnbCereal-Medium": require("@/assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Md.otf"),
+    "AirbnbCereal-Black": require("@/assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Blk.otf"),
+    "AirbnbCereal-ExtraBold": require("@/assets/fonts/airbnb-cereal-font/AirbnbCereal_W_XBd.otf"),
+
+    // Helvetica Neue (14 variants)
+    "HelveticaNeue-Black": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueBlack.otf"),
+    "HelveticaNeue-BlackItalic": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueBlackItalic.otf"),
+    "HelveticaNeue-Bold": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueBold.otf"),
+    "HelveticaNeue-BoldItalic": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueBoldItalic.otf"),
+    "HelveticaNeue-Heavy": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueHeavy.otf"),
+    "HelveticaNeue-HeavyItalic": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueHeavyItalic.otf"),
+    "HelveticaNeue-Italic": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueItalic.ttf"),
+    "HelveticaNeue-Light": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueLight.otf"),
+    "HelveticaNeue-LightItalic": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueLightItalic.otf"),
+    "HelveticaNeue-Medium": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueMedium.otf"),
+    "HelveticaNeue-MediumItalic": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueMediumItalic.otf"),
+    "HelveticaNeue-Roman": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueRoman.otf"),
+    "HelveticaNeue-Thin": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueThin.otf"),
+    "HelveticaNeue-ThinItalic": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueThinItalic.otf"),
+    "HelveticaNeue-UltraLight": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueUltraLight.otf"),
+    "HelveticaNeue-UltraLightItalic": require("@/assets/fonts/helvetica-neue-5/HelveticaNeueUltraLightItalic.otf"),
+  });
+
+  // Hide the splash screen once fonts are loaded
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null; // or a custom loading component
+  }
+
+  return <Stack />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+// import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function WelcomeScreen() {
